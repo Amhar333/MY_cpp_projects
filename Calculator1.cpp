f4e09d85a416b23499a2b25de1d58bd32ce8a281
Amhr <75868135+Amhar333@users.noreply.github.com>
2021-08-02 21:43:47 +0530

    Create Calculator1.cpp

---
+++ Calculator1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    double x, y;
+    cout << "x, y >" ;
+    cin >> x >> y;
+    cout << "x = " << x;
+    cout << "\ny = " << y;
+    cout << "\n\nx + y =" << (x + y) ;
+    cout << "\nx - y =" << (x - y) ;
+    cout << "\nx * y =" << (x * y) ;
+    cout << "\nx / y =" << (x / y);
+  return 0;
+
+}
