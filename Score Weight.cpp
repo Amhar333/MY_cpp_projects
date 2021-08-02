4e5dede1664c48655e55cede6488b5dcb6e57857
Amhr <75868135+Amhar333@users.noreply.github.com>
2021-08-02 10:54:24 +0530

    Create Score Weight.cpp

---
+++ Score Weight.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int num;
+    cout << "what is your score > ";
+    cin >> num;
+    cout << "weight of the scores is : \n";
+    if ((num >= 87) && (num <= 100))
+    {
+        cout << "4";
+    }
+    if ((num >= 74) && (num <= 86)){
+        cout << "3";
+    }
+    if ((num >= 60) && (num <= 73))
+    {
+        cout << "2";
+    }
+    if ((num >= 50) && (num <= 59))
+    {
+        cout << "1";
+    }
+    if ((num <= 49) && (num >= 0) || (num > 100)){
+        cout << "0";
+    }
+    if (num < 0){
+        cout << "INVALID SCORE! \nEnter valid scores (0-100)";
+    }
+    
+    return 0;
+}
