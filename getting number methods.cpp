aab8499cec52fc7ae7f3646347f836cd71b3d150
Amhr <75868135+Amhar333@users.noreply.github.com>
2021-08-28 21:58:36 +0530

    Update getting number methods.cpp

--- getting number methods.cpp
+++ getting number methods.cpp
@@ -6,7 +6,7 @@ using namespace std;
  
 int main() 
 {
-    int num, a, b, c, d, e, a2, b2, c2, d2, e2;
+    int num, a, b, c, d, e, a2, b2, c2, d2, e2, f2, g2, h2, i2, j2;
     cout << "Enter five digit number: ";
     cin >> num;
 //     num = 12345;
@@ -17,7 +17,7 @@ int main()
     d = num / 10 - (a * 1000 + b * 100 + c * 10);
     e = num / 1 - (a * 10000 + b * 1000 + c * 100 + d * 10);
     
-    
+    cout << "Method 1\n";
     cout << a << endl;
     cout << b << endl;
     cout << c << endl;
@@ -26,11 +26,11 @@ int main()
     
     if( (a < b) && (b < c) && (c < d) && (d < e))
     {
-        cout << "increasing order!\n";
+        cout << "increasing order!\n\n";
     }
     else if((e < d) && (d < c) && (c < b) && (b < a))
     {
-        cout << "Decreasing order\n";
+        cout << "Decreasing order\n\n";
     }
     else
     {
@@ -38,17 +38,22 @@ int main()
     }
 //     method 2 (getting number last to first)
     
-    a2 = num % 10 ;
-    b2 = num % 100;
-    c2 = num % 1000;
-    d2 = num % 10000;
-    e2 = num % 100000;
+    a2 = num % 10;
+    b2 = num / 10;
+    c2 = b2 % 10;
+    d2 = b2 / 10;
+    e2 = d2 % 10;
+    f2 = d2 / 10;
+    g2 = f2 % 10;
+    h2 = f2 / 10;
+    i2 = h2 % 10;
+    j2 = h2 / 10;
     
-    
-//     cout << a2 << endl;
-//     cout << b2 << endl;
-//     cout << c2 << endl;
-//     cout << d2 << endl;
-//     cout << e2 << endl;
+    cout << "Method 2\n";
+    cout << a2 << endl;
+    cout << c2 << endl;
+    cout << e2 << endl;
+    cout << g2 << endl;
+    cout << i2 << endl;
     return 0; 
 } /* main */ 
