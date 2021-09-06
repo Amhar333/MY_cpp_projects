1090574bd697c72499dfeaca07f23ae8b6c20296
Amhr <75868135+Amhar333@users.noreply.github.com>
2021-09-06 13:40:57 +0530

    Create my.cpp

---
+++ my.cpp
@@ -0,0 +1,26 @@
+// by thecodemonster
+
+#include <iostream>
+#include <cmath>
+#include <chrono>  // for high_resolution_clock
+using namespace std;
+
+// main function
+int main()
+{
+//     cout << "What is your name > ";
+//     string name;
+//     cin >> name;
+//     cout << "Assaalaamu alaikkum, \n" << name;
+    cout << "Enter a number > ";
+    int x;
+    cin >> x;
+    cout << "I set a formula that answer is only 5" << endl;
+    cout << "OK, let's begin...The Game..." << endl;
+    cout << "First you want to Multiply by 2 with your number";
+    
+    cout << ((2*x + 10 - x) / 2 - x / 2);
+    
+    return 0;
+} 
+// main function
